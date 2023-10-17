c70c5c4b4c25ef42e692107bd1091417aa2f35b4
ROHANI <rohani1996@gmail.com>
2023-10-17 17:24:48 -0400

    Add files via upload

--- removeElementArray.java
+++ removeElementArray.java
@@ -1,10 +1,34 @@
+//Write a Java program to remove a specific element from an array.
+
 package practiceQuestions;
 
-public class removeElementArray {
+public class removeElementArray 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		int a[] = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49}; //remove 18
+		
+		int elementToRemove = 18; 
+		int index=7; 
+		int temp=a[7];
+		int arraySize=a.length; 
+		
+		for(int i=index; i<a.length-1; i++)
+		{
+			a[i]=a[i+1]; 
+		}
+		a[arraySize-1] = 0; 
+		arraySize--;
+		
+		
+		for(int y:a)
+		{
+			System.out.print(y + " ");
+		}
+		
+		
+	
 	}
 
 }
